c608dcfb7515dfa9971aea8256bbdb9f9ac188ab
sebastian-jones <sebastianjones.dev@gmail.com>
2026-02-16 11:57:51 -0600

    Themes now utilize Islands

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,10 +1,1 @@
 rootProject.name = "Gruvbox - Islands"
-
-pluginManagement {
-    repositories {
-        maven {
-            url = uri("https://oss.sonatype.org/content/repositories/snapshots/")
-        }
-        gradlePluginPortal()
-    }
-}
